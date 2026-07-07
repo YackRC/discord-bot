c750f7bc04d3a1284d4eadd20397d9143de5fabd
Jack Ceballos <masteryack01@gmail.com>
2026-07-07 13:20:52 -0400

    pushing new index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
