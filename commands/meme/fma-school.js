0e8674ef1fca8af3d2e44aa5fe2af0431566903e
Jack Ceballos <masteryack01@gmail.com>
2026-07-26 14:14:55 -0400

    created subcommand for fma school

---
+++ commands/meme/fma-school.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('fma-school')
+		.setDescription('Must need clips from the martial arts legend.')
+	// Create subcommand
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('deathmatch')
+				.setDescription('Are you challenging me to a deathmatch?'),
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('tapi-tapi')
+				.setDescription('Learn the tapi tapi moves... Hadoken!'),
+		),
+
+	async execute(interaction) {
+		const subcommand = interaction.options.getSubcommand();
+
+		if (subcommand === 'deathmatch') {
+			await interaction.reply(
+				'https://www.youtube.com/shorts/u8nu-1SNXnc',
+			);
+		}
+
+		if (subcommand === 'tapi-tapi') {
+			await interaction.reply(
+				'https://www.youtube.com/shorts/e4M9ALg9F4o',
+			);
+		}
+	},
+};
