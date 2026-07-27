4320025939c064fcd7e08f4328e6a1d96e840268
Jack Ceballos <masteryack01@gmail.com>
2026-07-27 15:56:19 -0400

    perimeter.js added

--- commands/marathon/perimeter.js
+++ commands/marathon/perimeter.js
@@ -4,6 +4,7 @@ const { perimeter } = require('../../data/maps');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('perimeter')
+        .setDescription('Commands for Perimeter')
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('info')
