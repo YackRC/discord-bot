be0a65d5d3ff138ce87a1885d8e561274f1ade28
Jack Ceballos <masteryack01@gmail.com>
2026-07-27 15:58:24 -0400

    perimeter.js added

--- commands/marathon/perimeter.js
+++ commands/marathon/perimeter.js
@@ -4,24 +4,27 @@ const { perimeter } = require('../../data/maps');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('perimeter')
-		.setDescription('Commands for Perimeter')
-		.addSubcommand(subcommand =>
+		.setDescription('Commands for the Perimeter map.')
+		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('info')
-		        .setDescription('Basic info about Perimeter.'),
+				.setDescription('Display basic information about Perimeter.'),
 		),
+
 	async execute(interaction) {
 		const subcommand = interaction.options.getSubcommand();
 
 		if (subcommand === 'info') {
-			await interaction.reply([
-				`**${perimeter.name}**`,
-				`Threat Level: ${perimeter.threat}`,
-				`Squad Spawns: ${perimeter.squadSpawns}`,
-				`Solo Spawns: ${perimeter.soloSpawns}`,
-				`Points of Interest: ${perimeter.POIs.join(', ')}`,
-				`Priority Hostile: ${perimeter.prioHostileLocation}`,
-			].join('\n'));
+			await interaction.reply(
+				[
+					`**${perimeter.name}**`,
+					`Threat Level: ${perimeter.threat}`,
+					`Squad Spawns: ${perimeter.squadSpawns}`,
+					`Solo Spawns: ${perimeter.soloSpawns}`,
+					`Points of Interest: ${perimeter.POIs.join(', ')}`,
+					`Priority Hostile: ${perimeter.prioHostileLocation}`,
+				].join('\n'),
+			);
 		}
 	},
 };
